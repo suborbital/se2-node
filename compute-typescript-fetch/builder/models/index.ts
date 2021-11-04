288ba7448bd209b4548a5777d3e5b0763621cf46
Super Nova <60084237+MacklinEngineering@users.noreply.github.com>
2021-11-04 16:57:21 -0400

    raw output for generated typescript wrappers

---
+++ compute-typescript-fetch/builder/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './EditorState';
+export * from './InlineResponse200';
+export * from './InlineResponse2001';
+export * from './TestPayload';
