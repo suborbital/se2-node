1c96a854fc08aca620b844c4667bfa584d093e95
Oscar Spencer <oscar@grain-lang.org>
2021-11-05 12:19:45 -0400

    Add wrapper and project setup

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import * as administrative from "../compute-typescript-fetch/administrative"
+import * as builder from "../compute-typescript-fetch/builder"
+import * as execution from "../compute-typescript-fetch/execution"
+
+type config = {
+    baseUrl?: string;
+    accessToken: string;
+}
+
+interface suborbital {
+    administrative: administrative.DefaultApi;
+    builder: builder.DefaultApi;
+    execution: execution.DefaultApi;
+}
+
+export class Suborbital implements suborbital {
+    private _administrative: administrative.DefaultApi
+    private _builder: builder.DefaultApi
+    private _execution: execution.DefaultApi
+
+    constructor(options: config) {
+        this._administrative = new administrative.DefaultApi(new administrative.Configuration(options))
+        this._builder = new builder.DefaultApi(new builder.Configuration(options))
+        this._execution = new execution.DefaultApi(new execution.Configuration(options))
+    }
+
+    get administrative() {
+        return this._administrative
+    }
+
+    get builder() {
+        return this._builder
+    }
+
+    get execution() {
+        return this._execution
+    }
+}
