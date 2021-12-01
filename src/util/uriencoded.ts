a560d124680b6fb34ecf394998f057dd4ab801f7
Oscar Spencer <oscar@grain-lang.org>
2021-12-01 16:43:41 -0500

    Simplify code with better type info

--- src/util/uriencoded.ts
+++ src/util/uriencoded.ts
@@ -4,16 +4,12 @@ export default function uriencoded<Args, Result>(
   descriptor: TypedPropertyDescriptor<(arg1: object, ...rest: Args[]) => Result>
 ): TypedPropertyDescriptor<(arg1: object, ...rest: Args[]) => Result> {
   const originalMethod = descriptor.value;
-  descriptor.value = function (arg1: any, ...args: any[]) {
-    if (typeof arg1 == "object") {
-      const encodedParams = Object.keys(arg1).reduce((obj, key) => {
-        obj[key] = encodeURIComponent(arg1[key]);
-        return obj;
-      }, {});
-      return originalMethod.apply(this, [encodedParams, ...args]);
-    } else {
-      return originalMethod.apply(this, args);
-    }
+  descriptor.value = function (arg1: object, ...args: any[]) {
+    const encodedParams = Object.keys(arg1).reduce((obj, key) => {
+      obj[key] = encodeURIComponent(arg1[key]);
+      return obj;
+    }, {});
+    return originalMethod.apply(this, [encodedParams, ...args]);
   };
   return descriptor;
 }
